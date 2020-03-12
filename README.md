b0abd8bb95ec26f6bef68013b7b8d7c89fd67aeb
Francisco V <vxrnxk@gmail.com>
2020-03-12 08:53:30 -0300

    Autenticando o usuário

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Curso Alura PHP PDO parte 2: Mais segurança e organização na sua app
 - [Aula 03 - Usando serviços para exclusão](https://github.com/vxrnxk/cur-alura-laravel-parte2/tree/master/aula-03)
 - [Aula 04 - Edição da série](https://github.com/vxrnxk/cur-alura-laravel-parte2/tree/master/aula-04)
 - [Aula 05 - Assistindo episódios](https://github.com/vxrnxk/cur-alura-laravel-parte2/tree/master/aula-05)
+- [Aula 06 - Autenticando o usuário](https://github.com/vxrnxk/cur-alura-laravel-parte2/tree/master/aula-06)
---
+++ aula-06/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/series', 'SeriesController@index')
+    ->name('listar_series');
+Route::get('/series/criar', 'SeriesController@create')
+    ->name('form_criar_serie');
+Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+
+Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
+
+Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
+
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/entrar', 'EntrarController@index');
+Route::post('/entrar', 'EntrarController@entrar');
+Route::get('/registrar', 'RegistroController@create');
+Route::post('/registrar', 'RegistroController@store');
